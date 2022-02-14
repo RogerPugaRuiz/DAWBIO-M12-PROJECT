8c907186016c9faf5510407c95ee4773898c6b86
Roger Puga Ruiz <rogerpugaruiz@gmail.com>
2022-02-14 13:37:42 +0100

    solvent errors

---
+++ frontend/src/app/menu/menu.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+  path = {
+    img_src:"assets/images/logo.jpg"
+  };
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
