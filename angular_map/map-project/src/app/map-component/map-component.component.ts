99d2d6a1dde32c4b2d14a7439986e0ca8850b979
ivandmn <81816600+ivandmn@users.noreply.github.com>
2022-05-24 21:24:19 +0200

    Zoom Points

--- angular_map/map-project/src/app/map-component/map-component.component.ts
+++ angular_map/map-project/src/app/map-component/map-component.component.ts
@@ -25,6 +25,24 @@ export class MapComponentComponent implements OnInit {
   //Canary Islands Map size
   hCan: number = 120 ;
   wCan: number = 240;
+  scale: any  = {
+    1: 3,
+    2: 2.8,
+    3: 2.6,
+    4: 2.4,
+    5: 2.2,
+    6: 2, 
+    7: 1.8,
+    8: 1.6,
+    9: 1.4,
+    10: 1.2,
+    11: 1,
+    12: 0.9,
+    13: 0.8,
+    14: 0.7,
+    15: 0.5
+  } 
+  
 
     createMap() {
       //Peninsula Map
@@ -38,12 +56,37 @@ export class MapComponentComponent implements OnInit {
       let mapCan: any = d3.select("#mapC").append("svg").attr("id", "isleMap").attr("width", this.wCan).attr("height", this.hCan).style("border", "1px solid black").style("background-color", "#7fcdff").style("position", "absolute").style("top", "388px").style("left", "526px");
 
       //Zoom Functions
-      map = map.call(d3.zoom().scaleExtent([1,15]).translateExtent([[0,0],[this.wmap,this.hmap]]).on("zoom", function(){
+      map = map.call(d3.zoom().scaleExtent([1,15]).translateExtent([[0,0],[this.wmap,this.hmap]]).on("zoom", e => {
         map.attr("transform", d3.zoomTransform(map.node()))
+        if(e.sourceEvent.wheelDelta < 0){
+          console.log("zoom out")
+          let scale = d3.zoomTransform(map.node()).k;
+          let scale_int = Math.round(scale)
+          d3.selectAll('.circlesPeninsula').attr("r" , this.scale[scale_int]).attr("stroke-width", this.scale[scale_int])
+        } else {
+          let scale = d3.zoomTransform(map.node()).k;
+          let scale_int = Math.round(scale)
+          let actual_r = d3.selectAll('circle').attr('r')
+          d3.selectAll('.circlesPeninsula').attr("r" , this.scale[scale_int]).attr("stroke-width", this.scale[scale_int])
+
+        }
       })).append("g");
 
-      mapCan = mapCan.call(d3.zoom().scaleExtent([1,15]).translateExtent([[0,0],[this.wCan,this.hCan]]).on("zoom", function(){
+
+      mapCan = mapCan.call(d3.zoom().scaleExtent([1,15]).translateExtent([[0,0],[this.wCan,this.hCan]]).on("zoom", e =>{
         mapCan.attr("transform", d3.zoomTransform(mapCan.node()))
+        if(e.sourceEvent.wheelDelta < 0){
+          console.log("zoom out")
+          let scale = d3.zoomTransform(mapCan.node()).k;
+          let scale_int = Math.round(scale)
+          d3.selectAll('.circlesCan').attr("r" , this.scale[scale_int]).attr("stroke-width", this.scale[scale_int])
+        } else {
+          let scale = d3.zoomTransform(mapCan.node()).k;
+          let scale_int = Math.round(scale)
+          let actual_r = d3.selectAll('circle').attr('r')
+          d3.selectAll('.circlesCan').attr("r" , this.scale[scale_int]).attr("stroke-width", this.scale[scale_int])
+
+        }
       })).append("g");
 
       d3.json("https://unpkg.com/es-atlas@0.5.0/es/provinces.json").then(data => {
@@ -85,7 +128,7 @@ export class MapComponentComponent implements OnInit {
             .enter()
             .append("circle")
             .attr("id", element[0])
-            .attr("class", "circles")
+            .attr("class", "circlesPeninsula")
             // @ts-ignore
             .attr("cx", function(d){ return projection(d)[0] })
             // @ts-ignore
@@ -93,7 +136,7 @@ export class MapComponentComponent implements OnInit {
             .attr("r", 3)
             .style("fill", "black")
             .attr("stroke", "#69b3a2")
-            .attr("stroke-width", 2)
+            .attr("stroke-width", 3)
             .style('cursor', "pointer")
             .on("click", (event: any) => {
               console.log(event.target.id)
@@ -108,7 +151,7 @@ export class MapComponentComponent implements OnInit {
             .enter()
             .append("circle")
             .attr("id", element[0])
-            .attr("class", "circles")
+            .attr("class", "circlesCan")
             // @ts-ignore
             .attr("cx", function(d){return projectionCan(d)[0]})
             // @ts-ignore
@@ -116,7 +159,7 @@ export class MapComponentComponent implements OnInit {
             .attr("r", 3)
             .style("fill", "black")
             .attr("stroke", "#69b3a2")
-            .attr("stroke-width", 2)
+            .attr("stroke-width", 3)
             .style('cursor', "pointer")
             .on("click", function(event: any) {
               console.log(event.target.id)
